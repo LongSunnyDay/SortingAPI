fcbd5857cfe924f151dfb85af3ba9841feb68c53
Matas <avad47@gmail.com>
2020-11-15 09:18:16 +0200

    Add project files.

---
+++ OrderingApi/Controllers/SortingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+
+namespace Sorting.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SortingController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> SortNumbersList(int[] pNumbers)
+        {
+            try
+            {
+                if (pNumbers.Length == 0 || pNumbers == null)
+                {
+                    return BadRequest();
+
+                }
+                Models.Sorting vSorter = new Models.Sorting();
+                var vSortedArray = vSorter.SortNumbers(pNumbers);
+
+                return "Sorted array of numbers:" +
+                    Environment.NewLine + string.Join(", ", vSortedArray);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error during array sorting");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetLastSortRecord()
+        {
+            return "Hello there";
+        }
+    }
+}
